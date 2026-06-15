1d2f0db78c4f83fc8b43dbae55cbc792737d0576
JooeonLee <cacophony0609@gmail.com>
2026-06-15 16:15:13 +0900

    chore: add spring-boot-starter-actuator to all app modules
    - ledger-app, service-app, worker-app에 actuator 의존성 추가
    - /actuator/health 엔드포인트 활성화 (README 예시와 일치)
    - GlobalExceptionHandler에 NoResourceFoundException 핸들러 추가 (404 ERROR 로그 제거)

--- common/src/main/java/com/shinhan/eclipse/common/exception/GlobalExceptionHandler.java
+++ common/src/main/java/com/shinhan/eclipse/common/exception/GlobalExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @Slf4j
 @RestControllerAdvice
@@ -30,6 +31,13 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
                 .body(ApiResponse.error("C001", message));
+    }
+
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity<ApiResponse<Void>> handleNoResourceFound(NoResourceFoundException e) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(ApiResponse.error("C004", "요청한 리소스를 찾을 수 없습니다."));
     }
 
     @ExceptionHandler(Exception.class)
