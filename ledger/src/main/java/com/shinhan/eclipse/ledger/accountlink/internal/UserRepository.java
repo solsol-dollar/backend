691e91beb54a016863aa0a88bd8061de3e8da511
sukyungi <juli030109@naver.com>
2026-06-24 09:06:48 +0900

    fix: 누락된 accountlink í파일 삭제 반영(#53)

--- ledger/src/main/java/com/shinhan/eclipse/ledger/accountlink/internal/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.shinhan.eclipse.ledger.accountlink.internal;
-
-import com.shinhan.eclipse.domain.user.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-interface UserRepository extends JpaRepository<User, Long> {
-}
