2e5484dc48d05cbf43578cfac225981f5fde77b0
Sihyun Jang <jjj99nine2@gmail.com>
2026-06-26 15:54:40 +0900

    refactor: 뉴스 파이프라인 필터 개선

--- worker-app/src/test/java/com/shinhan/eclipse/worker/ipo/scheduler/IpoNewsSyncSchedulerTest.java
+++ worker-app/src/test/java/com/shinhan/eclipse/worker/ipo/scheduler/IpoNewsSyncSchedulerTest.java
@@ -1,10 +1,12 @@
 package com.shinhan.eclipse.worker.ipo.scheduler;
 
+import com.shinhan.eclipse.worker.ipo.repository.IpoNewsIpoRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.boot.test.context.runner.ApplicationContextRunner;
 import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
@@ -14,6 +16,8 @@ class IpoNewsSyncSchedulerTest {
 
     private final ApplicationContextRunner contextRunner = new ApplicationContextRunner()
             .withBean(JobLauncher.class, () -> mock(JobLauncher.class))
+            .withBean(IpoNewsIpoRepository.class, () -> mock(IpoNewsIpoRepository.class))
+            .withBean(TransactionTemplate.class, () -> mock(TransactionTemplate.class))
             .withUserConfiguration(IpoNewsSyncScheduler.class);
 
     @Test
